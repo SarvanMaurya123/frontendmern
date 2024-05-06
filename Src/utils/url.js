1daefe69fbd478f34a36387b8be19993e63ac7e2
Sarvan Maurya <sarvanmaurya037@gmail.com>
2024-05-06 16:29:41 +0530

    first commit

--- Src/utils/url.js
+++ Src/utils/url.js
@@ -1,1 +1,3 @@
-export const CORS_ORIGIN = "https://portfolio-ndz7iiqj1-sarvans-projects.vercel.app"
+//export const CORS_ORIGIN = "https://portfolio-ndz7iiqj1-sarvans-projects.vercel.app"
+
+export const CORS_ORIGIN = " http://localhost:5173"
